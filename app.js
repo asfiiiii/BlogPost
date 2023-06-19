d86bfbc26e60209885eacedee89d7ad95bd2aca3
Asfar <asfarma2815@gmail.com>
2023-06-19 23:55:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -191,6 +191,7 @@ app.delete("/home/:id",isLoggedIn,isAuthor, async(req,res)=>
   res.redirect('/home');
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
